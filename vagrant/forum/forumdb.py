3c6c46a10f394b444b1799463bd437bb1613099f
wsjr <wsjr@users.noreply.github.com>
2016-09-15 15:47:55 -0700

    Revert "Modify forumdb.py to use DB"

--- vagrant/forum/forumdb.py
+++ vagrant/forum/forumdb.py
@@ -1,24 +1,31 @@
 #
 # Database access functions for the web forum.
 # 
-import psycopg2
-import bleach
 
+import time
+
+## Database connection
+DB = []
 
 ## Get posts from database.
 def GetAllPosts():
-    DB = psycopg2.connect("dbname=forum")
-    c = DB.cursor()
-    c.execute("SELECT time, content FROM posts ORDER BY time DESC")
-    posts = [{'content': bleach.clean(str(row[1])), 'time': str(row[0])} for row in c.fetchall()]
-    DB.close()
+    '''Get all the posts from the database, sorted with the newest first.
+
+    Returns:
+      A list of dictionaries, where each dictionary has a 'content' key
+      pointing to the post content, and 'time' key pointing to the time
+      it was posted.
+    '''
+    posts = [{'content': str(row[1]), 'time': str(row[0])} for row in DB]
+    posts.sort(key=lambda row: row['time'], reverse=True)
     return posts
 
 ## Add a post to the database.
 def AddPost(content):
-    DB = psycopg2.connect("dbname=forum")
-    c = DB.cursor()
-    c.execute("INSERT INTO posts (content) VALUES (%s)", (content,))
-    DB.commit()
-    DB.close()
+    '''Add a new post to the database.
 
+    Args:
+      content: The text content of the new post.
+    '''
+    t = time.strftime('%c', time.localtime())
+    DB.append((t, content))
